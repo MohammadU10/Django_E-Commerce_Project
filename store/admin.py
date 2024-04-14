f153649764d29728e62129e5ccfdf5459c0e4a6e
Ninja.U102 <120134432+NinjaU10@users.noreply.github.com>
2024-04-14 13:50:15 +0330

    Update admin.py
    Registered `Cart` and `CartItem` models.

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Category, Brand, Spec, Item, ItemSpecValue
+from .models import Category, Brand, Spec, Item, ItemSpecValue, Cart, CartItem
 
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Spec)
 admin.site.register(Item)
 admin.site.register(ItemSpecValue)
+admin.site.register(Cart)
+admin.site.register(CartItem)
