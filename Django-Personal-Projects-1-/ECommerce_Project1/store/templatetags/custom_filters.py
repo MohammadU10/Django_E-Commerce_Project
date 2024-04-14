fb6c4646e4fab66eccaa52d11f868e1521683026
Ninja.U102 <120134432+NinjaU10@users.noreply.github.com>
2024-04-14 10:37:45 +0330

    Delete Django-Personal-Projects-1- directory

--- Django-Personal-Projects-1-/ECommerce_Project1/store/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from store.models import Spec
-
-register = template.Library()
-
-@register.filter
-def get_spec_by_name(spec_name):
-    return Spec.objects.get(name=spec_name)
