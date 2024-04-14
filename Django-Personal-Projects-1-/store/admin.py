e8ab2797fc405ed4e3802fb237622f1bc8f7080e
Ninja.U102 <120134432+NinjaU10@users.noreply.github.com>
2024-04-14 11:04:30 +0330

    Delete Django-Personal-Projects-1- directory

--- Django-Personal-Projects-1-/store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Brand, Spec, Item, ItemSpecValue
-
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Spec)
-admin.site.register(Item)
-admin.site.register(ItemSpecValue)
