6a3437a35d49c10e8a66031be168233b37e254d3
Ninja.U102 <120134432+NinjaU10@users.noreply.github.com>
2024-04-14 12:51:11 +0330

    Add files via upload

---
+++ store/templates/store/JS/store.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("main").style.marginLeft = "25%";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
