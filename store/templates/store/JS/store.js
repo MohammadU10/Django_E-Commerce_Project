5e7ff4db19878fe3a422a355c837451fb10dd2e3
Ninja.U102 <120134432+NinjaU10@users.noreply.github.com>
2024-04-16 12:58:58 +0330

    Update store.js
    Added JS related to Dark Mode Button

--- store/templates/store/JS/store.js
+++ store/templates/store/JS/store.js
@@ -1,9 +1,17 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "25%";
-  document.getElementById("main").style.marginLeft = "25%";
-}
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("main").style.marginLeft = "25%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
 
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
+
+const toggleElem = document.querySelector("#toggle");
+const toggleParent = document.querySelector(".wrap__toggle")
+toggleElem.addEventListener("change", () =>{
+
+    toggleParent.classList.toggle("active");
+});
